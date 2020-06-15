788cd2f04de49c85101381de55186bd252d32476
ElDonatello <aminebechar96@gmail.com>
2020-06-15 17:30:51 +0100

    bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class blog(models.Model):
-    title = models.TextField(max_length=20)
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
     date = models.DateField()
     body = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import *
+blogs = Blog.objects
 # Create your views here.
+def home(request):
+    return render(request,'home.html',dict(blogs=blogs))
